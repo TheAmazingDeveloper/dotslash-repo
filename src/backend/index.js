fe72d52ced8f8b1d828690dd4a94a4680b112ae4
Darshit Jain <darshitjain09092006@gmail.com>
2024-11-23 12:25:10 +0530

    Project Structure

---
+++ src/backend/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import mongoose from "mongoose";
+import __dirname from "./utils/directoryname.js";
+import cookieParser from "cookie-parser";
+dotenv.config({ path: "./.env" });
+
+// Initializing
+const app = express();
+
+// Enabling CORS
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+// Public folder to serve files
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+// IIFE to connect to MONGODB and listen to the server
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on http://localhost:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
---
+++ src/backend/utils/directoryname.js
@@ -0,0 +1,9 @@
+import path from "path";
+
+// 👇️ "/F:\Github Repos\dotslash-repo\backend\utils"
+const __filename = import.meta.dirname;
+
+// 👇️ "/F:\Github Repos\dotslash-repo\backend\"
+const __dirname = path.dirname(__filename);
+
+export default __dirname;
